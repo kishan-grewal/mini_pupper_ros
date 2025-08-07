42fcad8e6c36b304a3fda9cec3e0ffa2b84babcc
Kishan <kishangrewal06@gmail.com>
2025-08-07 11:52:54 +0800

    feat: finish accelerometer ekf update

--- mini_pupper_tracking_cpp/include/mini_pupper_tracking_cpp/lie_imu_node.hpp
+++ mini_pupper_tracking_cpp/include/mini_pupper_tracking_cpp/lie_imu_node.hpp
@@ -20,29 +20,37 @@ private:
     // topic /imu/data
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_data_subscription_;
     void imu_data_callback_ (sensor_msgs::msg::Imu::ConstSharedPtr msg);
+    rclcpp::Time last_imu_time_;
 
     // topic /cmd_vel
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscription_;
     void cmd_vel_callback_ (geometry_msgs::msg::Twist::ConstSharedPtr msg);
+    Eigen::Vector2d vel_;
 
     // EKF setup
     Eigen::Matrix4d X_; // R (phi) t (rho) 0v 1
     Matrix6d P_; // 6 = 3 from rho + 3 phi for a full twist [HOW CLEAR THE WINDSCREEN IS]
     Matrix6d Q_; // 6 same as above [WIND AND POTHOLES SO EXTERNAL FACTORS]
     // one R for each sensor
-    Matrix6d R_; // 6 same as above [SENSOR/MEASUREMENT NOISE]
+    Eigen::Matrix2d r_accel_; // 6 same as above [SENSOR/MEASUREMENT NOISE]
+    // H turns state information into measurement information
+    // if my state is this my measurement will probably be this
+    Eigen::Matrix<double, 2, 6> H_accel_;
+
+    // EKF functions
+    void predict_ (const double dt, const Vector6d& u);
+    void update_accel_ (const Eigen::Vector3d& accel);
+    void update_slam_ (const Eigen::Vector3d& slam_pos, double slam_yaw);
 
     // SE3 lie algebra
-    Eigen::Matrix3d skew_ (Eigen::Vector3d w);
-    Eigen::Vector3d unskew_ (Eigen::Matrix3d W);
-    Eigen::Matrix3d so3_exp_ (Eigen::Vector3d w);
-    Eigen::Vector3d so3_log_ (Eigen::Matrix3d R);
-    Eigen::Matrix3d so3_left_jacobian_ (Eigen::Vector3d w);
+    Eigen::Matrix3d skew_ (Eigen::Vector3d w); // gyro -> skew matrix of gyro
+    Eigen::Vector3d unskew_ (Eigen::Matrix3d W); // skew matrix of gyro -> gyro
+    Eigen::Matrix3d so3_exp_ (Eigen::Vector3d w); // gyro -> rotation mat
+    Eigen::Vector3d so3_log_ (Eigen::Matrix3d R); // rotation mat -> gyro
+    Eigen::Matrix3d so3_left_jacobian_ (Eigen::Vector3d w); 
     Eigen::Matrix3d so3_left_jacobian_inv_ (Eigen::Vector3d w);
-    Eigen::Matrix4d se3_exp_ (Vector6d xi);
-    Vector6d se3_log_ (Eigen::Matrix4d T);
-
-    void test_ ();
+    Eigen::Matrix4d se3_exp_ (Vector6d xi); // twist -> transformation
+    Vector6d se3_log_ (Eigen::Matrix4d T); // transformation -> twist
 };
 // CODE ABOVE ------------------------
 
--- mini_pupper_tracking_cpp/src/lie_imu_node.cpp
+++ mini_pupper_tracking_cpp/src/lie_imu_node.cpp
@@ -7,11 +7,108 @@ LieImuNode::LieImuNode()
 {
     RCLCPP_INFO(this->get_logger(), "LieImuNode has started.");
 
-    
+    imu_data_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
+        "/imu/data", 10,
+        std::bind(&LieImuNode::imu_data_callback_, this, std::placeholders::_1)
+    );
+    last_imu_time_ = this->now();
+
+    cmd_vel_subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+        "/cmd_vel", 10,
+        std::bind(&LieImuNode::cmd_vel_callback_, this, std::placeholders::_1)
+    );
+    vel_ = Eigen::Vector2d::Zero();
+
+    X_ = Eigen::Matrix4d::Identity();
+    P_ = Matrix6d::Identity();
+    Q_ = Matrix6d::Identity() * 1e-3;
+    r_accel_ = Eigen::Matrix2d::Identity() * 1e-2;
+    H_accel_ = Eigen::Matrix<double, 2, 6>::Zero();
+    H_accel_(0, 3) = 1.0;
+    H_accel_(1, 4) = 1.0;
 }
 
 // (void)msg
 // INFO_STREAM
+
+void LieImuNode::imu_data_callback_ (sensor_msgs::msg::Imu::ConstSharedPtr msg)
+{   
+    rclcpp::Time current_time(msg->header.stamp);
+    const double dt = (current_time - last_imu_time_).seconds();
+    last_imu_time_ = current_time;
+
+    Eigen::Vector3d accel;
+    accel << msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z;
+    Eigen::Vector3d gyro;
+    gyro << msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z;
+
+    Vector6d u;
+    u << vel_(0), vel_(1), 0.0, gyro(0), gyro(1), gyro(2);
+
+    predict_(dt, u);
+    update_accel_ (accel);
+
+    // Quick logging
+    Eigen::Vector3d pos = X_.block<3,1>(0,3);
+    Eigen::Matrix3d R = X_.block<3,3>(0,0);
+    Eigen::Vector3d rpy = so3_log_(R);
+    RCLCPP_INFO(this->get_logger(), 
+        "Pos: [%.3f, %.3f, %.3f] RPY: [%.3f, %.3f, %.3f]", 
+        pos(0), pos(1), pos(2), 
+        rpy(0)*180/M_PI, rpy(1)*180/M_PI, rpy(2)*180/M_PI);
+}
+
+void LieImuNode::cmd_vel_callback_ (geometry_msgs::msg::Twist::ConstSharedPtr msg)
+{
+    vel_ << msg->linear.x, msg->linear.y;
+}
+
+void LieImuNode::predict_ (const double dt, const Vector6d& u)
+{
+    Vector6d xi = u * dt;
+    Eigen::Matrix4d T = se3_exp_(xi);
+    // world->new = world->old * old->new
+    X_ = X_ * T;
+
+    Matrix6d F = Matrix6d::Identity();
+    // update covariance with continuous Q
+    P_ = F * P_ * F.transpose() + Q_ * dt;
+}
+
+void LieImuNode::update_accel_ (const Eigen::Vector3d& accel) 
+{
+    double roll_measured = atan2(accel(1), 
+                                accel(2));
+    double pitch_measured = atan2(-accel(0), 
+    sqrt(accel(1) * accel(1) + accel(2) * accel(2)));
+    Eigen::Vector2d z;
+    z << roll_measured, pitch_measured; // r then p then y
+
+    Eigen::Matrix3d R = X_.block<3, 3>(0, 0);
+    Eigen::Vector3d gyro_R = so3_log_(R);
+    Eigen::Vector2d h_hat;
+    h_hat << gyro_R(0), gyro_R(1); // roll and pitch
+
+    // innovation
+    Eigen::Vector2d y = z - h_hat;
+    // y = z - hhat
+
+    // current innovation covariance
+    Eigen::Matrix2d S = H_accel_ * P_ * H_accel_.transpose() + r_accel_;
+    // {6,6}*{6,2}*{2,2} = {6,2}
+    Eigen::Matrix<double, 6, 2> K = P_ * H_accel_.transpose() * S.inverse();
+
+    // SE(3): x̂⁺ = x̂⁻ ⊞ δx instead of x̂⁺ = x̂⁻ + δx
+    // x = x + ky
+    X_ = X_ * se3_exp_(K * y);
+
+    // covariance counter-update:
+    Matrix6d I = Matrix6d::Identity();
+    //P_ *= (I - K*H_);
+    // {6,6} = {6,6} - {6,2}*{2,6}
+    Matrix6d KH = K * H_accel_;
+    P_ = (I - KH) * P_ * (I - KH).transpose() + K * r_accel_ * K.transpose(); // joseph
+}
 
 Eigen::Matrix3d LieImuNode::skew_ (Eigen::Vector3d phi)
 {
@@ -131,17 +228,3 @@ Vector6d LieImuNode::se3_log_ (Eigen::Matrix4d T)
     xi.tail<3>() = phi;
     return xi;
 }
-
-void LieImuNode::test_ ()
-{
-    Vector6d xi;
-    xi << 0.1, -0.2, 0.05, 0.01, 0.02, 0.03;
-
-    Eigen::Matrix4d T = se3_exp_(xi);
-    Vector6d xi_recovered = se3_log_(T);
-
-    std::cout << "Original xi:      " << xi.transpose() << std::endl;
-    std::cout << "Recovered xi:     " << xi_recovered.transpose() << std::endl;
-    std::cout << "Difference:       " << (xi - xi_recovered).transpose() << std::endl;
-}
-
